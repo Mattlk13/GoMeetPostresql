c2fc688193f15b42ca1957a4d65de60e1456efec
imRohan <rlikhite@gmail.com>
2017-02-28 16:11:09 -0500

    started working on connections

---
+++ database_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "rohan_test"
+)
