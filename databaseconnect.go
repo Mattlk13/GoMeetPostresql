029971cb9a6fda5a526c78761f7e491a6cd8d019
imRohan <rlikhite@gmail.com>
2017-03-01 15:12:32 -0500

    DRY pattern observed, cleaned up errors and made code modular

--- databaseconnect.go
+++ databaseconnect.go
@@ -5,7 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	/* use reflect to find type of vars */
-	"reflect"
+	/*"reflect"*/
 	/* use lib/pq as a postgres driver */
 	_ "github.com/lib/pq"
 )
@@ -19,47 +19,64 @@ const (
 	dbname   = "postgres"
 )
 
-func query_database(query_string string) *sql.Rows {
-	/* connection string */
-	connection_string := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-
-	/* validate connection to pg */
-	db, err := sql.Open("postgres", connection_string)
-
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-	defer db.Close()
-
+func query_database(db *sql.DB, query_string string) *sql.Rows {
 	/* open a connection to the database */
-	err = db.Ping()
+	err := db.Ping()
 	if err != nil {
-		fmt.Println("Error: ", err)
+		fmt.Println("Error!")
+		panic(err)
 	}
-
-	fmt.Println("Successfully connected!")
 
 	/* Query Database */
 	returned_output, err := db.Query(query_string)
 	if err != nil {
-		fmt.Println("Error: ", err)
+		fmt.Println("Error!")
+		panic(err)
 	}
 	defer returned_output.Close()
 
 	/* Output result */
-
-	fmt.Println(reflect.TypeOf(returned_output))
 	return returned_output
 }
 
-func insert_into_database(insert_string string) {
+func create_timecard(db *sql.DB, id int, username string) {
+	insert_statement := fmt.Sprintf("INSERT INTO timecards (ID, USERNAME, OCCURRENCE) VALUES(%d, '%s', current_timestamp)", id, username)
 
+	/* open a connection to the database */
+	err := db.Ping()
+	if err != nil {
+		fmt.Println("Error!")
+		panic(err)
+	}
+
+	/* Prepare Insertion Database */
+	insert_command, err := db.Prepare(insert_statement)
+	if err != nil {
+		fmt.Println("Error!")
+		panic(err)
+	}
+
+	/* Execute Insert Statement */
+	_, err = insert_command.Exec()
+	if err != nil {
+		fmt.Println("Error!")
+		panic(err)
+	} else {
+		fmt.Println("Successfully Inserted")
+	}
+
+	defer insert_command.Close()
+}
+
+func main() {
 	/* connection string */
 	connection_string := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
+
+	/* Query Strings */
+	query_all := "SELECT * FROM timecards"
+	query_one := "SELECT * FROM timecards WHERE(Id = 1)"
 
 	/* validate connection to pg */
 	db, err := sql.Open("postgres", connection_string)
@@ -69,40 +86,14 @@ func insert_into_database(insert_string string) {
 	}
 	defer db.Close()
 
-	/* open a connection to the database */
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	fmt.Println("Successfully connected!")
-
-	/* Insert into Database */
-	insert_statement, err := db.Prepare(insert_string)
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	_, err = insert_statement.Exec()
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}else{
-		fmt.Println("Successfully Inserted")
-	}
-}
-
-func main() {
-	query_all := "SELECT * FROM timecards"
-	query_one := "SELECT * FROM timecards WHERE(Id = 1)"
-	insert_one := "INSERT INTO timecards (ID, USERNAME, OCCURRENCE) VALUES(5, 'RICKY', current_timestamp)"
 
 	/* connect to database and load into var */
-	list_of_timecards := query_database(query_all)
-	first_timecard := query_database(query_one)
+	list_of_timecards := query_database(db, query_all)
+	first_timecard := query_database(db, query_one)
 
 	fmt.Println(list_of_timecards)
 	fmt.Println(first_timecard)
 
-	insert_into_database(insert_one)
+	create_timecard(db, 9, "DOUG")
 
 }
